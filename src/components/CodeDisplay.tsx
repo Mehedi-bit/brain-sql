2b5ddd80800001e500cc954b0994067debf2b261
Md. Mehedi Hasan Emon <60240261+Mehedi-bit@users.noreply.github.com>
2023-04-29 09:36:52 +0600

    BrainSQL Intro

---
+++ src/components/CodeDisplay.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CodeDisplay = () => {
+  return (
+    <div className="code-display">
+      <p>Code display area</p>
+    </div>
+  );
+};
+
+export default CodeDisplay;
---
+++ src/components/MessageDisplay.tsx
@@ -0,0 +1,9 @@
+const MessageDisplay = () => {
+  return (
+    <div className="message-display">
+      <p>Message display area</p>
+    </div>
+  );
+};
+
+export default MessageDisplay;
